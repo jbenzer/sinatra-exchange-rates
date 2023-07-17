0977e4965aa7680f47a324a4d1fcc2aff65a3ff0
jbenzer <136658052+jbenzer@users.noreply.github.com>
2023-07-17 20:10:32 +0000

    fix gem

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 # we need to require this gem to open a json URL
 require 'open-uri'
+require 'http'
+require 'json'
 
 symbols_url = HTTP.get("https://api.exchangerate.host/symbols")
 parsed_response = JSON.parse(symbols_url)
