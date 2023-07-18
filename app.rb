2264d58dc22b08701bc34c8221b51dd73beedc76
jbenzer <136658052+jbenzer@users.noreply.github.com>
2023-07-18 18:24:37 +0000

    fuck bob

--- app.rb
+++ app.rb
@@ -4,15 +4,16 @@ require 'open-uri'
 require 'http'
 require 'json'
 
-symbols_url = HTTP.get("https://api.exchangerate.host/symbols")
-parsed_response = JSON.parse(symbols_url)
-symbols = parsed_response.fetch("symbols")
-$symbol = []
-symbols.each do |key, value|
-  $symbol.push(key)
-end
+
 
 get('/') do
+  symbols_url = HTTP.get("https://api.exchangerate.host/symbols")
+  parsed_response = JSON.parse(symbols_url)
+  symbols = parsed_response.fetch("symbols")
+  $symbol = []
+  symbols.each do |key, value|
+    $symbol.push(key)
+  end
   erb(:symbol_list)
 end
 
